190a381be6c3a0a3ff1c5efc571789c242d8bc49
kinakomoch7 <book-insect14@outlook.jp>
2025-02-23 18:05:59 +0900

    ごめ、えらってた

--- wakeupquiz/App.js
+++ wakeupquiz/App.js
@@ -2,8 +2,10 @@ import Home from "./src/pages/Home";
 import Problem from "./src/pages/Problem";
 import Result from "./src/pages/Result";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
