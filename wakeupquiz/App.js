8e65f3e4365aa796bcad9bc704787cc50e16fe5d
kinakomoch7 <book-insect14@outlook.jp>
2025-02-23 16:23:38 +0900

    アイコン設定してなかっためんご

--- wakeupquiz/App.js
+++ wakeupquiz/App.js
@@ -1,17 +1,38 @@
 import Problem from "./src/pages/Problem";
 import Result from "./src/pages/Result";
+import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStaticNavigation } from "@react-navigation/native";
+import Feather from "@expo/vector-icons/Feather";
+import Foundation from "@expo/vector-icons/Foundation";
 
-const MyTabs = createBottomTabNavigator({
-  screens: {
-    Problem: Problem,
-    Result: Result,
-  },
-});
-
-const Navigation = createStaticNavigation(MyTabs);
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Problem"
+          component={Problem}
+          options={{
+            title: "問題",
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="book-open" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Result"
+          component={Result}
+          options={{
+            title: "成績",
+            tabBarIcon: ({ color, size }) => (
+              <Foundation name="results" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
