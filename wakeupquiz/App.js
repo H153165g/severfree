afb85b1a98733ca4cfc70a1c836d6b126265215c
kinakomoch7 <book-insect14@outlook.jp>
2025-02-23 18:04:32 +0900

    戻し

--- wakeupquiz/App.js
+++ wakeupquiz/App.js
@@ -1,38 +1,16 @@
+import Home from "./src/pages/Home";
 import Problem from "./src/pages/Problem";
 import Result from "./src/pages/Result";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feather from "@expo/vector-icons/Feather";
-import Foundation from "@expo/vector-icons/Foundation";
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Problem"
-          component={Problem}
-          options={{
-            title: "問題",
-            tabBarIcon: ({ color, size }) => (
-              <Feather name="book-open" size={size} color={color} />
-            ),
-          }}
-        />
-
-        <Tab.Screen
-          name="Result"
-          component={Result}
-          options={{
-            title: "成績",
-            tabBarIcon: ({ color, size }) => (
-              <Foundation name="results" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Problem" component={Problem} />
+        <Stack.Screen name="Result" component={Result} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
